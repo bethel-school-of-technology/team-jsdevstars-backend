a2a870259de1dac3e4f33a4ff43b337259ed63d1
Felipe Sarres <sarres@gmail.com>
2023-01-23 21:13:24 -0600

    added index.ts

---
+++ src/models/index.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import { UserFactory } from "./user";
+import { TweetFactory } from "./tweet";
+import { AssociateUserTweets } from "./tweet";
+
+const dbName = 'twitterDB';
+const username = 'root';
+const password = 'Password1!';
+
+const sequelize = new Sequelize(dbName, username, password, {
+host: 'localhost',
+port: 3306,
+dialect: 'mysql'
+});
+
+TweetFactory(sequelize);
+UserFactory(sequelize);
+AssociateUserTweets();
+
+export const db = sequelize;
