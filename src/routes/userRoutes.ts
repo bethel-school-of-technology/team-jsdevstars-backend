cb78d87210f9dac2c86ba94d7b68c984b617fab7
Brett <bdmdiving@gmail.com>
2023-02-05 16:23:09 +1100

    deleted articleComment route file and cleaned up some code from tweet project example

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-// import { getPublicUserTweets, getloggedinTweets } from '../controllers/tweetController';
-import { createUser, deleteUser, editUser, getUser, loginUser } from '../controllers/userController';
+import { createUser, deleteUser, getUser, loginUser } from '../controllers/userController';
 
 const router = Router();
 
@@ -10,16 +9,8 @@ router.post('/login', loginUser);
 
 router.get('/:userId', getUser);
 
-router.put('/:userId', editUser);
+// router.put('/:userId', editUser);
 
 router.delete('/:userId', deleteUser);
-
-// correct path in postman
-// router.get('/:userId', getUserComments);
-
-// not working in postman
-// router.get('/getLoggedinTweets', getloggedinTweets)
-
-// router.get('/logged/:userId', getloggedinTweets)
 
 export default router;
