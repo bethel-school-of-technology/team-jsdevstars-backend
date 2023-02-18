f0c55b9628689fb4cd7787770e66d94d29e46440
Brett <bdmdiving@gmail.com>
2023-02-18 16:02:59 +1100

    getting user name to show from local storage

--- build/controllers/userController.js
+++ build/controllers/userController.js
@@ -65,7 +65,7 @@ const loginUser = async (req, res, next) => {
         // If passwords match, create a JWT
         if (passwordsMatch) {
             let token = await (0, auth_1.signUserToken)(existingUser);
-            res.status(200).json({ token });
+            res.status(200).json({ token: token, user: existingUser });
         }
         else {
             res.status(401).json('Invalid password');
